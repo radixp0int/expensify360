2c7055372291fd5c87c4992f5c2f43077cbe34ee
amckann <azm6424@psu.edu>
2021-10-14 11:35:40 -0400

    added CSRF_FAILURE_VIEW setting

--- Expensify360/Expensify360/settings.py
+++ Expensify360/Expensify360/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_FAILURE_VIEW = 'django.views.csrf.csrf_failure'
