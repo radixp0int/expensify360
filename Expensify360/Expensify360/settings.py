d68b5e81895f0107c12ca7e8a4138ac366cfa206
amckann <azm6424@psu.edu>
2021-10-20 20:11:26 -0400

    Fixed urls

--- Expensify360/Expensify360/settings.py
+++ Expensify360/Expensify360/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Dashboard',
+    'Expenses',
+    'Pages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
