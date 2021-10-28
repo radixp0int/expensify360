aaa8ba6f8c565698d19b1c85ef5671b5443dbdff
amckann <azm6424@psu.edu>
2021-10-27 22:20:46 -0400

    added dynamic organization, team member list to dash

--- Expensify360/Dashboard/views.py
+++ Expensify360/Dashboard/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import Group
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def homepage(request):
-    return render(request=request, template_name='homepage.html')
+    context = {}
+    groups = list(request.user.groups.values_list('name', flat=True))
+    for g in groups:
+        context[g] = list(Group.objects.get(id=g).user_set.all())
+    return render(request=request, template_name='homepage.html', context=context)
