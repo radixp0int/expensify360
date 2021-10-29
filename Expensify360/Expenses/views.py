0c3f85fbea6f8361b3e0ec6f343b719dba2901f7
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-10-28 22:02:56 -0400

    Began to implement the expense display handling

--- Expensify360/Expenses/views.py
+++ Expensify360/Expenses/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import *
 
 
 def expense(request):
-    return render(request, 'expense.html')
+    mileageForm = MileageForm(request.POST or None)
+    expenseForm = ExpenseForm(request.POST or None)
+    hoursForm = HoursForm(request.POST or None)
+
+    context = {
+        'mileageForm': mileageForm,
+        'expenseForm': expenseForm,
+        'hoursForm': hoursForm,
+    }
+    return render(request, 'expense.html', context)
