4fd6ae0d65f5cd8fcac150b1b3dc47919a267cbb
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-10-28 20:28:07 -0400

    Added models for the Mileage, Expense, and Hours fields

--- Expensify360/Expenses/models.py
+++ Expensify360/Expenses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    # General
+    userID = models.CharField(max_length=151)
+    expenseDate = models.DateField()
+    submissionDate = models.DateField(auto_now_add=True)
+
+    # Mileage Specific
+    miles = models.DecimalFIeld(max_digits=8, decimal_places=1)
+    mileageRate = models.DecimalFIeld(max_digits=5, decimal_places=2)
+    milageTotal = models.DecimalFIeld(max_digits=8, decimal_places=2)
+
+    # Expense Specific
+    # TO-DO FIlE UPLOAD HERE
+    expenseCost = models.DecimalFIeld(max_digits=8, decimal_places=2)
+    tax = models.DecimalFIeld(max_digits=8, decimal_places=2)
+    shipping = models.DecimalFIeld(max_digits=8, decimal_places=2)
+    expenseTotal = models.DecimalFIeld(max_digits=8, decimal_places=2)
+
+    # Hours Specific
+    hours = models.DecimalFIeld(max_digits=6, decimal_places=2)
+    hourlyRate = models.DecimalFIeld(max_digits=6, decimal_places=2)
+    hourTotal = models.DecimalFIeld(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.userID)
