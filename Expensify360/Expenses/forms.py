d550e3c6fc7704d88a1be827faafac87eb853e0f
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-10-28 22:04:09 -0400

    Added the 3 primary forms

--- Expensify360/Expenses/forms.py
+++ Expensify360/Expenses/forms.py
@@ -1,3 +1,20 @@
 from django import forms
 from .models import Request
 
+
+class MileageForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields = ('expenseDate', 'miles', 'mileageRate', 'mileageTotal')
+
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields = ('expenseDate', 'expenseCost', 'tax', 'shipping', 'expenseTotal')
+
+
+class HoursForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields = ('expenseDate', 'hours', 'hourlyRate', 'hourTotal')
