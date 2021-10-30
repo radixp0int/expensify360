78f57d28c23f3a8367302b9bac955520f2b53967
amckann <azm6424@psu.edu>
2021-10-30 15:26:22 -0400

    added success page

--- Expensify360/Dashboard/urls.py
+++ Expensify360/Dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
-    path('create_organization/', views.create_org, name='create_org')
+    path('create_organization/', views.create_org, name='create_org'),
+    path('success/', views.org_success, name='org_success')
     ]
--- Expensify360/Dashboard/views.py
+++ Expensify360/Dashboard/views.py
@@ -1,19 +1,29 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Dashboard.forms import *
 
 
 @login_required
 def homepage(request):
     context = {}
-    groups = list(request.user.groups.values_list('name', flat=True))
+    groups = request.user.groups.all()
     for g in groups:
-        context[g] = list(Group.objects.get(id=g).user_set.all())
+        context[g] = list(Group.objects.get(id=g.id).user_set.all())
     return render(request=request, template_name='homepage.html', context=context)
 
 
+@login_required
 def create_org(request):
     if request.method == 'POST':
-        return render(request, 'homepage.html')
+        form = CreateOrgForm(request.POST)
+        if form.is_valid():
+            org = Group(name=form.cleaned_data['Organization_Name'])
+            org.save()
+            request.user.groups.add(org)
+        return redirect('org_success')
     return render(request, 'create_org.html', {'form': CreateOrgForm()})
+
+
+def org_success(request):
+    return render(request, template_name='org_success.html')
