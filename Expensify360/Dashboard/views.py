9e01eef9632f8bd2a36a246f2cca49960191482c
amckann <azm6424@psu.edu>
2021-10-30 14:42:18 -0400

    added create new org page

--- Expensify360/Dashboard/views.py
+++ Expensify360/Dashboard/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 from django.shortcuts import render
+from Dashboard.forms import *
 
 
 @login_required
@@ -10,3 +11,9 @@ def homepage(request):
     for g in groups:
         context[g] = list(Group.objects.get(id=g).user_set.all())
     return render(request=request, template_name='homepage.html', context=context)
+
+
+def create_org(request):
+    if request.method == 'POST':
+        return render(request, 'homepage.html')
+    return render(request, 'create_org.html', {'form': CreateOrgForm()})
