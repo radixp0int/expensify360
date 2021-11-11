e85032e14581b5ffdc900513bcac35a9354e48ac
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-11-10 22:24:08 -0500

    Added the submission handling and form clearing function.

--- Expensify360/Expenses/views.py
+++ Expensify360/Expenses/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+
+from .models import *
 from .forms import *
 
 import datetime
@@ -16,10 +18,25 @@ def expense(request):
         if form.is_valid():
             userID = form.cleaned_data['userID']
             expenseDate = form.cleaned_data['expenseDate']
+            submissionDate = form.cleaned_data['submissionDate']
+            organization = form.cleaned_data['organization']
+            project = form.cleaned_data['project']
+            miles = form.cleaned_data['miles']
+            mileageRate = form.cleaned_data['mileageRate']
+            mileageTotal = form.cleaned_data['mileageTotal']
 
+            mileageInfo = Expense(userID=userID,
+                                  expenseDate=expenseDate,
+                                  submissionDate=submissionDate,
+                                  organization=organization,
+                                  project=project,
+                                  miles=miles,
+                                  mileageRate=mileageRate,
+                                  mileageTotal=mileageTotal,)
+            mileageInfo.save()
             print(userID, expenseDate)
-    else:
-        form = mileageEntryForm(initial={'userID': current_user,
+
+    form = mileageEntryForm(initial={'userID': current_user,
                                      'expenseDate': today})
 
     context = {
