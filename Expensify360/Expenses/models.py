98ca4a6d4726e40c7cdafa01e2197caaa8abf9a3
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-11-10 22:46:14 -0500

    Added null fields to the expense model to accomadate different form types along with the isApproved field having it default to pending.

--- Expensify360/Expenses/models.py
+++ Expensify360/Expenses/models.py
@@ -1,30 +1,31 @@
 from django.db import models
 
 
-class Request(models.Model):
+class Expense(models.Model):
     # General
     userID = models.CharField(max_length=151)
     expenseDate = models.DateField()
     submissionDate = models.DateField(auto_now_add=True)
     organization = models.CharField(max_length=151)
     project = models.CharField(max_length=151)
+    isApproved = models.CharField(max_length=50, default="Pending")
 
     # Mileage Specific
-    miles = models.DecimalField(max_digits=8, decimal_places=1)
-    mileageRate = models.DecimalField(max_digits=5, decimal_places=2)
-    mileageTotal = models.DecimalField(max_digits=8, decimal_places=2)
+    miles = models.DecimalField(max_digits=8, decimal_places=1, null=True, default=None)
+    mileageRate = models.DecimalField(max_digits=5, decimal_places=2, null=True, default=None)
+    mileageTotal = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
 
     # Expense Specific
     # TO-DO FIlE UPLOAD HERE
-    expenseCost = models.DecimalField(max_digits=8, decimal_places=2)
-    tax = models.DecimalField(max_digits=8, decimal_places=2)
-    shipping = models.DecimalField(max_digits=8, decimal_places=2)
-    expenseTotal = models.DecimalField(max_digits=8, decimal_places=2)
+    expenseCost = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
+    tax = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
+    shipping = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
+    expenseTotal = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
 
     # Hours Specific
-    hours = models.DecimalField(max_digits=6, decimal_places=2)
-    hourlyRate = models.DecimalField(max_digits=6, decimal_places=2)
-    hourTotal = models.DecimalField(max_digits=6, decimal_places=2)
+    hours = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
+    hourlyRate = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
+    hourTotal = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
 
     def __str__(self):
         return str(self.userID)
