05093c7c68f3e4ac3ad11340321ca0a038db0e9c
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-11-11 22:05:31 -0500

    Added new URL Patterns for additional expense entry pages

--- Expensify360/Expenses/urls.py
+++ Expensify360/Expenses/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('expense/', views.expense, name='expense')
+    path('expense/', views.expense, name='expense'),
+    path('expense/MileageEntry', views.mileageEntry, name='mileageEntry'),
+    path('expense/ExpenseEntry', views.expenseEntry, name='expenseEntry'),
+    path('expense/TimeEntry', views.timeEntry, name='timeEntry'),
 ]
