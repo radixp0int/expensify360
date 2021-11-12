735a2b2ce8dc0b44ff2c1a044e2349666d1dea0e
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-11-11 20:03:32 -0500

    Added expense and mileage entry form

--- Expensify360/Expenses/forms.py
+++ Expensify360/Expenses/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Field, HTML
+from crispy_forms.bootstrap import FormActions
+
 
 
 class mileageEntryForm(forms.Form):
@@ -10,3 +15,27 @@ class mileageEntryForm(forms.Form):
     miles = forms.DecimalField(label='Miles Driven')
     mileageRate = forms.DecimalField(label='Rate Per Mile')
     mileageTotal = forms.DecimalField(label='Total Cost')
+
+
+class expenseEntryForm(forms.Form):
+    userID = forms.CharField(label='User ID')
+    expenseDate = forms.DateField(label='Expense Date')
+    submissionDate = forms.DateField(label='Submission Date')
+    organization = forms.CharField(label='Organization')
+    project = forms.CharField(label='Project')
+    filePath = forms.FileField(label='Receipt Upload')
+    expenseCost = forms.DecimalField(label='Item Cost')
+    tax = forms.DecimalField(label='Tax')
+    shipping = forms.DecimalField(label='Shipping Cost')
+    expenseTotal = forms.DecimalField(label='Total Cost')
+
+
+class timeEntryForm(forms.Form):
+    userID = forms.CharField(label='User ID')
+    expenseDate = forms.DateField(label='Expense Date')
+    submissionDate = forms.DateField(label='Submission Date')
+    organization = forms.CharField(label='Organization')
+    project = forms.CharField(label='Project')
+    hours = forms.DecimalField(label='Hours Worked')
+    hourlyRate = forms.DecimalField(label='Hourly Rate')
+    hourTotal = forms.DecimalField(label='Total Cost')
