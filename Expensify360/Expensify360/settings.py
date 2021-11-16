73b0abfe9579847c64cb372301fa8f768305c191
Christian Sifuentes <chris.f.sifuentes@gmail.com>
2021-11-16 08:52:45 -0500

    refactor(): Adding main.css, images for expense tiles, and static files reference in settings.py

--- Expensify360/Expensify360/settings.py
+++ Expensify360/Expensify360/settings.py
@@ -125,7 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
