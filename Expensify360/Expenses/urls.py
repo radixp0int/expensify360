8ce102523911580b9352968b8eb4236c2c3f1fb0
Christian Sifuentes <chris.f.sifuentes@gmail.com>
2021-11-24 18:01:41 -0500

    EXP-55 | feature(): Moving expense_approval.

--- Expensify360/Expenses/urls.py
+++ Expensify360/Expenses/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('expense/MileageEntry', views.mileageEntry, name='mileageEntry'),
     path('expense/ExpenseEntry', views.expenseEntry, name='expenseEntry'),
     path('expense/TimeEntry', views.timeEntry, name='timeEntry'),
-    path('expense_approval/', views.expense_approval, name='expense_approval'),
 ]
