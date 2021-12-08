7270c13cc36e1630632d13b5ac6cb0ee036aae8e
dominic24515 <59629447+dominic24515@users.noreply.github.com>
2021-12-07 22:51:51 -0500

    Increased max digits in model to 30 to eliminate possibility of exceeding it in an expense.

--- Expensify360/Expenses/models.py
+++ Expensify360/Expenses/models.py
@@ -12,21 +12,21 @@ class Expense(models.Model):
     expenseType = models.CharField(max_length=50, default="", null=True)
 
     # Mileage Specific
-    miles = models.DecimalField(max_digits=8, decimal_places=1, null=True, default=None)
-    mileageRate = models.DecimalField(max_digits=5, decimal_places=2, null=True, default=None)
-    mileageTotal = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
+    miles = models.DecimalField(max_digits=30, decimal_places=1, null=True, default=None)
+    mileageRate = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
+    mileageTotal = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
 
     # Expense Specific
     expensePhoto = models.FileField(null=True, default=None)
-    expenseCost = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
-    tax = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
-    shipping = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
-    expenseTotal = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=None)
+    expenseCost = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
+    tax = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
+    shipping = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
+    expenseTotal = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
 
     # Hours Specific
-    hours = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
-    hourlyRate = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
-    hourTotal = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None)
+    hours = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
+    hourlyRate = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
+    hourTotal = models.DecimalField(max_digits=30, decimal_places=2, null=True, default=None)
 
     def __str__(self):
         return str(self.userID)
